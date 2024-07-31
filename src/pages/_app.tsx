c30e2580f2b7896e897bb5d1e4d09afd8130c19c
Lanh <hophuoclanh@gmail.com>
2024-07-31 12:21:01 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Roboto } from "next/font/google";
+
+const rb = Roboto({ subsets: ["latin"],weight: ["400"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={rb.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return(
+        <>
+            <h1>hello dashboard</h1>
+        </>
+    )
+}
+
+export default Dashboard;
