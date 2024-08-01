a11a4ba53e5b63442e94e4aa8d0dee9ec530bccc
Lanh <hophuoclanh@gmail.com>
2024-08-01 16:44:06 +0700

    loginfooter

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -4,6 +4,15 @@ const LogIn = () =>{
     const [isShow, setIsShow] = useState(false);
     return(
         <div>
+            <div>
+                <span>ĐĂNG NHẬP HỆ THỐNG</span>
+            </div>
+            <div>
+                <span>Quản lý cấp trường</span>
+            </div>
+            <div>
+                <span>TÀI KHOẢN CỦA BẠN</span>
+            </div>
         </div>
     )
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,15 +6,17 @@ import { useState } from "react";
 const LogInPage = () => {
     const [isShow,setIsShow]= useState<boolean>(false);
     return(
-        <div className="flex flex-col relative h-screen">
-            <LogInHeader/>
-            <div className="flex justify-items-center m-40">
-                <img src="/assets/image/database.png" alt="database" />
+        <div className="flex flex-col h-screen">
+            <LogInHeader />
+            <div className="flex flex-grow justify-around items-center w-full px-10">
+                <div className="flex justify-end w-1/2">
+                    <img src="/assets/image/database.png" alt="database" className="max-w-full h-auto" />
+                </div>
+                <div className="flex justify-start w-1/2">
+                    <LogIn />
+                </div>
             </div>
-            <LogIn/>
-            <div className="-mt-20">
-                <LogInFooter/>
-            </div>
+            <LogInFooter />
         </div>
     )
 }
